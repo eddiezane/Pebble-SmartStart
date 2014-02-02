375ee9c98b23abd3a251ec0459e5ffef72b85a2c
Eddie Zaneski <eddiezane@gmail.com>
2014-02-02 17:03:23 -0500

    Initial commit

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,78 @@
+var options;
+
+Pebble.addEventListener("ready", function(e) {
+  console.log("Ready");
+  if(window.localStorage.getItem('username') && 
+     window.localStorage.getItem('password')) {
+       options = {
+         'username': window.localStorage.getItem('username'),
+         'password': window.localStorage.getItem('password')
+       }
+       options.sessid = doLogin();
+     } else {
+       Pebble.showSimpleNotificationOnPebble("SmartStart", "Please login through settings page!");
+     }
+});
+
+Pebble.addEventListener("showConfiguration", function() {
+  console.log("Config opened");
+  Pebble.openURL("https://eddiezane.ngrok.com");
+});
+
+Pebble.addEventListener("webviewclosed", function(e) {
+  console.log("Config closed");
+  options = JSON.parse(decodeURIComponent(e.response));
+  window.localStorage.setItem('username', options.username);
+  window.localStorage.setItem('password', options.password);
+  doLogin();
+});
+
+Pebble.addEventListener("appmessage", function(e) {
+  console.log("Received message: " + JSON.stringify(e.payload.command));
+  switch(e.payload.command) {
+    case 0:
+      fireCommand("arm", options.sessid);
+      break;
+    case 1:
+      fireCommand("disarm", options.sessid);
+      break;
+    case 2:
+      fireCommand("remote", options.sessid);
+      break;
+  }
+});
+
+function doLogin() {
+  console.log("Inside doLogin");
+  var res;
+  var sessid;
+  var req = new XMLHttpRequest();
+  req.open('GET', 'https://colt.calamp-ts.com/auth/login/' + options.username + '/' + options.password, false);
+  req.onload = function(e) {
+    console.log("Inside onload");
+    if(req.responseText === undefined) {
+      Pebble.showSimpleNotificationOnPebble("SmartStart", "Error logging in, try again!");
+      sessid = null;
+      return null
+    }
+    res = JSON.parse(req.responseText);
+    sessid = res.Return.Results.SessionID
+  }
+  req.send(null);
+  return sessid;
+}
+
+function fireCommand(command, token) {
+  console.log("Inside fireCommand");
+  if(options.sessid == null) {
+    return null;
+  }
+  var req = new XMLHttpRequest();
+  req.open('GET', 'https://colt.calamp-ts.com/device/sendcommand/43853/'+ command + '?sessid=' + token, true);
+  req.onload = function(e) {
+    console.log("Inside onload");
+    res = JSON.parse(req.responseText);
+    console.log(res.Return.ResponseSummary.StatusCode);
+  }
+  req.send(null);
+}
---
+++ src/pebble_smartstart.c
@@ -0,0 +1,107 @@
+#include <pebble.h>
+
+typedef enum {
+  ARM,
+  DISARM,
+  REMOTE
+} COMMAND;
+
+static Window *window;
+static TextLayer *text_layer;
+
+void out_sent_handler(DictionaryIterator *sent, void *context) {
+  // outgoing message was delivered
+}
+
+
+void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
+  // outgoing message failed
+}
+
+
+void in_received_handler(DictionaryIterator *received, void *context) {
+  // incoming message received
+}
+
+
+void in_dropped_handler(AppMessageResult reason, void *context) {
+  // incoming message dropped
+}
+
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(text_layer, "Select");
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+  Tuplet value = TupletInteger(0, REMOTE);
+  dict_write_tuplet(iter, &value);
+  app_message_outbox_send();
+}
+
+static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(text_layer, "Up");
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+  Tuplet value = TupletInteger(0, DISARM);
+  dict_write_tuplet(iter, &value);
+  app_message_outbox_send();
+}
+
+static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(text_layer, "Down");
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+  Tuplet value = TupletInteger(0, ARM);
+  dict_write_tuplet(iter, &value);
+  app_message_outbox_send();
+}
+
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+}
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
+  text_layer_set_text(text_layer, "Press a button");
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+}
+
+static void window_unload(Window *window) {
+  text_layer_destroy(text_layer);
+}
+
+static void init(void) {
+  window = window_create();
+  window_set_click_config_provider(window, click_config_provider);
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+  const bool animated = true;
+  window_stack_push(window, animated);
+  app_message_register_inbox_received(in_received_handler);
+  app_message_register_inbox_dropped(in_dropped_handler);
+  app_message_register_outbox_sent(out_sent_handler);
+  app_message_register_outbox_failed(out_failed_handler);
+  const uint32_t inbound_size = 64;
+  const uint32_t outbound_size = 64;
+  app_message_open(inbound_size, outbound_size);
+}
+
+static void deinit(void) {
+  window_destroy(window);
+}
+
+int main(void) {
+  init();
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+
+  app_event_loop();
+  deinit();
+}
